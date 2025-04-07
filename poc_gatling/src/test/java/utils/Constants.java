ea5c4c4943638022dc7604bd02806436b0f96eda
Bruno Longo <bruno.longo.ltda@gmail.com>
2025-04-07 15:09:30 -0300

    ReqRes Constants File add

--- poc_gatling/src/test/java/utils/Constants.java
+++ poc_gatling/src/test/java/utils/Constants.java
@@ -8,13 +8,18 @@ public class Constants {
     public static final int TIMEOUT = 5000;
     public static String REQRES_BASE_URL = "https://reqres.in/";
     public static final Map<String, String> REQRES_HEADERS;
-
     static {
         Map<String, String> headers = new HashMap<>();
         headers.put("sec-ch-ua", "\"Google Chrome\\\";v=\\\"135\\\", \\\"Not-A.Brand\\\";v=\\\"8\\\", \\\"Chromium\\\";v=\\\"135\\\"");
         headers.put("sec-ch-ua-mobile", "?0");
         headers.put("sec-ch-ua-platform", "Windows");
-
-        REQRES_HEADERS = Collections.unmodifiableMap(headers); // deixa imutável
+        REQRES_HEADERS = Collections.unmodifiableMap(headers);
+    }
+    public static final Map<String, String> REQRES_ENDPOINTS;
+    static {
+        Map<String, String> endpoints = new HashMap<>();
+        endpoints.put("users", "api/users");
+        endpoints.put("unknown", "api/unknown");
+        REQRES_ENDPOINTS = Collections.unmodifiableMap(endpoints);
     }
 }
--- poc_gatling/src/test/scala/reqres/getListUsers.scala
+++ poc_gatling/src/test/scala/reqres/getListUsers.scala
@@ -14,23 +14,24 @@ class getListUsers extends Simulation {
 		.userAgentHeader("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36")
 
 	val headers: Map[String, String] = Constants.REQRES_HEADERS.asScala.toMap
+	val endPoints: Map[String, String] = Constants.REQRES_ENDPOINTS.asScala.toMap
 
-	val scn = scenario("getListUsers")
-		.exec(http("request_0")
-			.get("/api/users?page=2")
+	val scn = scenario("USERS_VALIDATION")
+		.exec(http("Get List Users for page 2")
+			.get(endPoints("users") + "?page=2")
 			.headers(headers))
 		.pause(1)
 		.exec(http("request_1")
-			.get("/api/users/2")
+			.get(endPoints("users") + "/2")
 			.headers(headers))
 		.pause(1)
 		.exec(http("request_2")
-			.get("/api/users/23")
+			.get(endPoints("users") + "/23")
 			.headers(headers)
 			.check(status.is(404)))
 		.pause(1)
 		.exec(http("request_3")
-			.get("/api/unknown")
+			.get(endPoints("unknown"))
 			.headers(headers))
 
 	setUp(scn.inject(atOnceUsers(1))).protocols(httpProtocol)
